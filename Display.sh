911c2878db227fd55f7be0a822e193b93ef2fd9d
tharinduudu <59080668+tharinduudu@users.noreply.github.com>
2025-11-21 18:03:33 -0500

    Update Display.sh

--- Display.sh
+++ Display.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /home/cosmic/mppcinterface-oct-2022/firmware/libraries/slowControl
+cd /home/cosmic/mppcInterface/firmware/libraries/slowControl
 tail -f $(ls -t | head -n 1)
